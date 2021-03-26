a27355ae9045f54c3db5ff4282f99a69e05e9da2
barendan <barendan@gmail.com>
2021-03-26 13:04:13 -0400

    cleanup code

--- App.js
+++ App.js
@@ -7,18 +7,10 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.greeting}>{t('greeting-text')}</Text>
+      <Text>{t('greeting-text')}</Text>
       <View style={styles.btnContainer}>
-        <Button
-          onPress={() => i18n.changeLanguage('ru')}
-          title="Russian"
-          style={styles.btn}
-        />
-        <Button
-          onPress={() => i18n.changeLanguage('en')}
-          title="English"
-          style={styles.btn}
-        />
+        <Button onPress={() => i18n.changeLanguage('ru')} title="Russian" />
+        <Button onPress={() => i18n.changeLanguage('en')} title="English" />
       </View>
     </View>
   );
@@ -31,16 +23,10 @@ const styles = StyleSheet.create({
     marginLeft: 50,
     marginRight: 50,
   },
-  greeting: {
-    paddingBottom: 20,
-  },
   btnContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-  },
-  btn: {
-    width: 50,
-    // paddingRight: 20,
+    paddingTop: 20,
   },
 });
 
